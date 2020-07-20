715814c4746a4fc389895550f6087ca29577b566
feryal abdullah <feryalabdullah26@gmail.com>
2020-07-20 21:11:08 +0300

    Add code for change Images

--- js/country.js
+++ js/country.js
@@ -19,3 +19,43 @@ main.addEventListener('change', function(){
       sub.appendChild(option);
    });
 });
+
+function jsDropDown(img_size, folder, img_content, img_id){
+   //document.getElementById(img_id).src = folder + "/" + img_content + ".jpg";
+
+   var cities = document.getElementsByClassName('city');
+   var selected_city = sub.value;
+   imgs = document.getElementsByClassName('img-city');
+   // if (cities == "Ibb") {
+   //    document.getElementById(img_id).src = folder + "/" + img_content + "1"  +".jpg"; 
+   // }
+   var string = "";
+   if (selected_city == "Ibb") {
+      console.info("Ibb");
+      string = "ibb";
+  } 
+  else if (selected_city == "Sana\'a") {
+   console.info("Sana\'a");
+   string = "sana\'a";
+  }
+  else if(selected_city == "Socatra"){
+   string = "socatra";
+  } 
+  else if(selected_city == "Cairo"){
+   string = "cairo";
+  } 
+  else if(selected_city == "Jeza"){
+   string = "jeza";
+  } 
+  else if(selected_city == "Alexandria"){
+   string = "alexandria";
+  } 
+  else{
+     string = "";
+  }
+   for(i = 1; i < imgs.length + 1; i++){
+      // imgs.src = "img" + "/" + "ibb1"  +".jpg";
+      // document.getElementsByTagName(img).style.display = "block";
+      document.getElementById(img_id + i).src = "img" + "/" + string + i +".jpg"; 
+   }
+}
