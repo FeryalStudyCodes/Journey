aeb08a7cb92c06ef730beab54b684e77788bfab5
feryal abdullah <feryalabdullah26@gmail.com>
2020-07-20 21:27:55 +0300

    Add some modifications

--- js/country.js
+++ js/country.js
@@ -1,26 +1,26 @@
 var countries = {
-        Yemen:['Sana\'a', 'Ibb', 'Socatra'],
-        Egypt:['Cairo', 'Jeza', 'Alexandria']
+   Yemen: ['Sana\'a', 'Ibb', 'Socatra'],
+   Egypt: ['Cairo', 'Jeza', 'Alexandria']
 }
 
 var main = document.getElementById('main_menu');
 var sub = document.getElementById('sub_menu');
 
-main.addEventListener('change', function(){
+main.addEventListener('change', function () {
    var selected_option = countries[this.value];
 
-   while(sub.options.length > 0){
-       sub.options.remove(0);
+   while (sub.options.length > 0) {
+      sub.options.remove(0);
    }
 
-   Array.from(selected_option).forEach(function(el){
+   Array.from(selected_option).forEach(function (el) {
       let option = new Option(el, el);
 
       sub.appendChild(option);
    });
 });
 
-function jsDropDown(img_size, folder, img_content, img_id){
+function jsDropDown(img_size, folder, img_content, img_id) {
    //document.getElementById(img_id).src = folder + "/" + img_content + ".jpg";
 
    var cities = document.getElementsByClassName('city');
@@ -33,29 +33,25 @@ function jsDropDown(img_size, folder, img_content, img_id){
    if (selected_city == "Ibb") {
       console.info("Ibb");
       string = "ibb";
-  } 
-  else if (selected_city == "Sana\'a") {
-   console.info("Sana\'a");
-   string = "sana\'a";
-  }
-  else if(selected_city == "Socatra"){
-   string = "socatra";
-  } 
-  else if(selected_city == "Cairo"){
-   string = "cairo";
-  } 
-  else if(selected_city == "Jeza"){
-   string = "jeza";
-  } 
-  else if(selected_city == "Alexandria"){
-   string = "alexandria";
-  } 
-  else{
-     string = "";
-  }
-   for(i = 1; i < imgs.length + 1; i++){
-      // imgs.src = "img" + "/" + "ibb1"  +".jpg";
-      // document.getElementsByTagName(img).style.display = "block";
-      document.getElementById(img_id + i).src = "img" + "/" + string + i +".jpg"; 
+   } else if (selected_city == "Sana\'a") {
+      console.info("Sana\'a");
+      string = "sana\'a";
+   } else if (selected_city == "Socatra") {
+      string = "socatra";
+   } else if (selected_city == "Cairo") {
+      string = "cairo";
+   } else if (selected_city == "Jeza") {
+      string = "jeza";
+   } else if (selected_city == "Alexandria") {
+      string = "alexandria";
+   } else {
+      string = "";
    }
+   for (i = 1; i < imgs.length + 1; i++) {
+      document.getElementById(img_id + i).src = "img" + "/" + string + i + ".jpg";
+   }
+   // var x = document.getElementsByClassName("city");
+   // if (x.style.display === "none") {
+   //    x.style.display = "block";
+   // }
 }
